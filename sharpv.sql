98259956674fe89ef4dffd9a8a25d7e1a44957d3
gromit <spatriotc@sina.com>
2017-04-04 17:09:58 +0800

    Create table sharpv_email_verification and complete email verification mapper.

--- sharpv.sql
+++ sharpv.sql
@@ -1,113 +1,71 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 03, 2017 at 08:35 AM
--- Server version: 10.1.10-MariaDB
--- PHP Version: 7.0.3
+/*
+Navicat MySQL Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+Source Server         : localhost_3306
+Source Server Version : 50711
+Source Host           : localhost:3306
+Source Database       : sharpv
 
+Target Server Type    : MYSQL
+Target Server Version : 50711
+File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+Date: 2017-04-04 17:01:00
+*/
 
---
--- Database: `sharpv`
---
+SET FOREIGN_KEY_CHECKS=0;
 
--- --------------------------------------------------------
+-- ----------------------------
+-- Table structure for sharpv_email_verification
+-- ----------------------------
+DROP TABLE IF EXISTS `sharpv_email_verification`;
+CREATE TABLE `sharpv_email_verification` (
+  `email` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `expire_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `token` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Table structure for table `sharpv_users`
---
-
+-- ----------------------------
+-- Table structure for sharpv_users
+-- ----------------------------
+DROP TABLE IF EXISTS `sharpv_users`;
 CREATE TABLE `sharpv_users` (
-  `user_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_group_id` int(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `sharpv_users`
---
-
-INSERT INTO `sharpv_users` (`user_id`, `username`, `password`, `email`, `user_group_id`) VALUES
-(0, 'Guest', '', 'noreply@sharp-v.org', 2),
-(1, 'root', 'e118b111376cffc0fcfb10e9dc43884b', 'webmaster@sharp-v.org', 3),
-(2, 'hzxie', '785ee107c11dfe36de668b1ae7baacbb', 'cshzxie@gmail.com', 2),
-(3, 'sunce', 'e10adc3949ba59abbe56e057f20f883e', 'spatriotc@gmail.com', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sharpv_user_groups`
---
-
-CREATE TABLE `sharpv_user_groups` (
   `user_group_id` int(4) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `user_group_id` (`user_group_id`),
+  CONSTRAINT `sharpv_users_ibfk_1` FOREIGN KEY (`user_group_id`) REFERENCES `sharpv_user_groups` (`user_group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of sharpv_users
+-- ----------------------------
+INSERT INTO `sharpv_users` VALUES ('0', 'Guest', '', 'guest@sharp-v.org', '2');
+INSERT INTO `sharpv_users` VALUES ('1', 'root', 'e118b111376cffc0fcfb10e9dc43884b', 'webmaster@sharp-v.org', '3');
+INSERT INTO `sharpv_users` VALUES ('2', 'hzxie', '785ee107c11dfe36de668b1ae7baacbb', 'cshzxie@gmail.com', '2');
+INSERT INTO `sharpv_users` VALUES ('3', 'sunce', 'e10adc3949ba59abbe56e057f20f883e', 'spatriotc@gmail.com', '2');
+
+-- ----------------------------
+-- Table structure for sharpv_user_groups
+-- ----------------------------
+DROP TABLE IF EXISTS `sharpv_user_groups`;
+CREATE TABLE `sharpv_user_groups` (
+  `user_group_id` int(4) NOT NULL AUTO_INCREMENT,
   `user_group_slug` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_group_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `user_group_name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `sharpv_user_groups`
---
-
-INSERT INTO `sharpv_user_groups` (`user_group_id`, `user_group_slug`, `user_group_name`) VALUES
-(1, 'forbidden', 'Forbidden'),
-(2, 'users', 'Users'),
-(3, 'administrators', 'Administrators');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `sharpv_users`
---
-ALTER TABLE `sharpv_users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `user_group_id` (`user_group_id`);
-
---
--- Indexes for table `sharpv_user_groups`
---
-ALTER TABLE `sharpv_user_groups`
-  ADD PRIMARY KEY (`user_group_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `sharpv_users`
---
-ALTER TABLE `sharpv_users`
-  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `sharpv_user_groups`
---
-ALTER TABLE `sharpv_user_groups`
-  MODIFY `user_group_id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `sharpv_users`
---
-ALTER TABLE `sharpv_users`
-  ADD CONSTRAINT `sharpv_users_ibfk_1` FOREIGN KEY (`user_group_id`) REFERENCES `sharpv_user_groups` (`user_group_id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records of sharpv_user_groups
+-- ----------------------------
+INSERT INTO `sharpv_user_groups` VALUES ('1', 'forbidden', 'Forbidden');
+INSERT INTO `sharpv_user_groups` VALUES ('2', 'users', 'Users');
+INSERT INTO `sharpv_user_groups` VALUES ('3', 'administrators', 'Administrators');
